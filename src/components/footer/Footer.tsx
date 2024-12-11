4d06c55b094b882e73e01a3ccbc89715d42d4ef9
Fabiana Balduína <fabiana.silva1@estudante.ifb.edu.br>
2024-12-11 19:12:40 -0300

    Primeiro commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyledFooter } from './styles'
+
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      Desenvolvido com 🤍 por Fabiana Balduína!
+    </StyledFooter>
+  );
+};
+
+export default Footer;
---
+++ src/components/footer/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledFooter = styled.footer`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgb(40, 36, 71);
+  color: white;
+  text-align: center;
+  padding: 1rem;
+`;
